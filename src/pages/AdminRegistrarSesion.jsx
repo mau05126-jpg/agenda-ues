3a0b86d0924e0699448c3d2562317ee0af6f605e
MauSR <mau05126@gmail.com>
2026-06-02 20:38:49 -0600

    feat: agregar opcion Inauguracion en tipo de sesion

--- src/pages/AdminRegistrarSesion.jsx
+++ src/pages/AdminRegistrarSesion.jsx
@@ -252,7 +252,7 @@ const AdminRegistrarSesion = ({ setCurrentPage }) => {
 
   const audienceOptions = ['Sistemas', 'Innovación Agrícola', 'Contaduría', 'Público general', 'Comunidad UES'];
   const dateOptions = ['01', '02', '03', '04', '05'];
-  const tipoSesionOptions = ['Conferencia', 'Taller Práctico'];
+  const tipoSesionOptions = ['Conferencia', 'Taller Práctico', 'Inauguración'];
   const escenarioOptions = ['Aula Magna', 'Laboratorio de Cómputo', 'Plazoleta Institucional'];
 
   if (!user || user.rol !== 'admin') {
