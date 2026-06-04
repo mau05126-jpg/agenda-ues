d5d72e12c461f65bedc407693d65b5ec1b5b11a4
MauSR <mau05126@gmail.com>
2026-06-04 16:04:26 -0600

    fix: excluir html5-qrcode de optimizeDeps para compatibilidad con Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['html5-qrcode'],
+  },
   server: {
     proxy: {
       '/api': {
